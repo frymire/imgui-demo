c318e65f218a193df16c366094cb3fa43454eadb
Mark Frymire <mark.e.frymire@wbbinc.com>
2022-03-01 10:56:49 -0500

    Demonstrate imgui with a custom window

---
+++ examples/example_win32_directx12/Application.cpp
@@ -0,0 +1,21 @@
+#include "Application.h"
+#include "imgui.h"
+
+namespace MyApp {
+
+    void RenderUI() {
+        ImGui::Begin("Settings");
+        ImGui::Button("Hello");
+        static float value = 0.0f;
+        ImGui::DragFloat("Value", &value);
+        ImGui::End();
+
+        // Leave the demo window in to browse design options during debugging.
+        // Search for corresponding text labels in imgui_demo.cpp (e.g. "Basic trees").
+        // NOTE: This causes errors when used with a dockspace.
+#ifdef _DEBUG
+        ImGui::ShowDemoWindow();
+#endif
+    }
+
+}
